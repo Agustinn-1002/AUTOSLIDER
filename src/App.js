5e67d87036a09b6e8a5f2c089b73e8ef7d7797bf
Generico Infoandina <agupasten@gmail.com>
2023-03-14 16:41:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {datos} from './data/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="hero-left App">
+        <div className='layers'>
+          {datos.map(e=>
+            <div className='layer layer-displayed'>
+                <span>{e.titulo}</span>
+                <div
+                  className='layer-image'
+                  style={{backgroundImage: `url(${e.image})`}}
+                ></div>
+                <div className='layer-info'>
+                  <div>
+                    <strong>Size</strong>
+                    <strong>Shape</strong>
+                    <strong>Camber</strong> d
+                    <strong>Width</strong>
+                    <strong>Flex</strong>
+                    <strong>Sidecut</strong>
+                  </div>
+                  <span>{e.size}</span>
+                  <span>{e.shape}</span>
+                  <span>{e.camber}</span>
+                  <span>{e.width}</span>
+                  <span>{e.flex}</span>
+                  <span>{e.sidecut}</span>
+                </div>
+            </div>
+          )}
+        </div>
+        <button>{'<-'}</button>
+        <button>{'->'}</button>
+      </div>
+      <div className='hero'></div>
+    </main>
   );
 }
 
---
+++ src/data/index.js
@@ -0,0 +1,44 @@
+export const datos = [
+    {
+        titulo:'THE CANNON',
+        size: '123cm',
+        shape: 'TAPPERED SWALLONTAIL',
+        camber: 'TRUE',
+        width: 'MID-WIDE',
+        flex: 'ALL TERRAIN',
+        sidecut:'PROGESSIVE',
+        image: 'https://pngimg.com/uploads/snowboard/snowboard_PNG8008.png',
+        slideOne: 'https://images.unsplash.com/photo-1495468286609-71018e87abc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=987c55a292efaa0adb9acfc26c06e22a&auto=format&fit=crop&w=670&q=80',
+        slideTwo: 'https://images.unsplash.com/photo-1486335223442-a034129506f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=564f090b8a99c189845f2ef71d4f620a&auto=format&fit=crop&w=1350&q=80',
+        autor: 'BRION FOX',
+        subTittle: 'RIDING THE CANNON'
+    },
+    {
+        titulo:'THE POW',
+        size: '110cm',
+        shape: 'TWIN',
+        camber: 'FALSE',
+        width: 'WIDE',
+        flex: 'ALL TERRAIN',
+        sidecut:'PROGESSIVE',
+        image: 'https://pngimg.com/uploads/snowboard/snowboard_PNG8006.png',
+        slideOne: 'https://images.unsplash.com/photo-1536099876051-79f4cbffeed1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=451b9215ee8acc20d5d7ea354aab570e&auto=format&fit=crop&w=1350&q=80',
+        slideTwo: 'https://images.unsplash.com/photo-1522056615691-da7b8106c665?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4f51ed1121800f23da98176659ba7506&auto=format&fit=crop&w=1352&q=80',
+        autor: 'AUSTIN SMITH',
+        subTittle: 'RIDING THE POW'
+    }
+    ,{
+        titulo:'THE CANNON',
+        size: '123cm',
+        shape: 'TAPPERED SWALLONTAIL',
+        camber: 'TRUE',
+        width: 'MID-WIDE',
+        flex: 'ALL TERRAIN',
+        sidecut:'PROGESSIVE',
+        image: 'https://pngimg.com/uploads/snowboard/snowboard_PNG7998.png',
+        slideOne: 'https://images.unsplash.com/photo-1512821062947-6eda5253c3e2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=305dedf0e09581de0ee68ab2c9523955&auto=format&fit=crop&w=1351&q=80',
+        slideTwo: 'https://images.unsplash.com/photo-1478700485868-972b69dc3fc4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=918195bdc5a37a2f412cf49234183427&auto=format&fit=crop&w=1350&q=80',
+        autor: 'BRION FOX',
+        subTittle: 'RIDING THE CANNON'
+    },
+]
